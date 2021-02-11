6100d8b2f3a79ea0fc8d4b6920acdb28d9d600b4
Olga <olgagarcher@gmail.com>
2021-02-11 08:37:19 +0200

    Organized folders

--- Spring/restaurant/src/main/java/com/olgibaba/restaurant/persistence/OrderedProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.olgibaba.restaurant.persistence;
-
-import com.olgibaba.restaurant.buisness.entities.OrderedProducts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderedProductsRepository extends JpaRepository<OrderedProducts, Integer> {
-}
